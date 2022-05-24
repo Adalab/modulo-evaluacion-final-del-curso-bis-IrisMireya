16036ab6ed02b2e44d780929723758054fb32268
Iris Mireya <irismzb_86@hotmail.com>
2022-05-24 20:23:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Objetivos:
+Recuperar el modulo 2, Java Script
+
+
+Planificación del proyecto
+Uso de HTML y maquetación con CSS.
+Uso del Adalab Web Started Kit (partials, anidación…).
+Gestión de eventos en el navegador: al hacer click, recargar, búsqueda errónea.
+Acceso y envío de datos a un servidor.
+Almacenamiento en local usando LocalStorage.
+
+Construido con:
+Lenguajes:
+HTML SCSS JAVASCRIPT
+
+Herramientas:
+VSC GITHUB GULP ADALAB WEB STARTER KIT NPM
+Autores:
+Iris M. Zuñel Ballesteros
---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let recipes = [];
+
+const listRecipes = document.querySelector(".js-recipes");
+const saveDatabtn = document.querySelector(".js-saveData");
+const loadDatabtn = document.querySelector(".js-loadData");
+
+function getData() {
+  fetch("https://api.sampleapis.com/recipes/recipes")
+    .then((response) => response.json())
+    .then(function (data) {
+      saveRecipes(data);
+      paintRecipes(recipes);
+    })
+     .catch(showError);
+     function showError() {
+    alert("Datos no disponibles");
+    listRecipes.innerHTML = `<button class="reload" onclick=location.reload()>Recargar</button>`;
+  }
+
+   /* function saveRecipes() {
+      localStorage.setItem("recipes", JSON.stringify(recipes));
+    }
+
+    function loadRecipes() {
+      listRecipes.innerHTML = "";
+      let saveRecipes = JSON.parse(localStorage.getItem("recipes"));
+      paintUsers(saveUsers);
+      let totalFriends = 0;
+      for (let cont = 0; cont < users.length; cont++) {
+        const isFriend = users[cont].isFriend;
+        if (isFriend === true) { 
+          totalFriends++;
+        }
+      }
+    }
+  }
+
+  function saveRecipes(data) {
+    const datos = data.results;
+    for (const dato of datos) {
+      let recipes = {
+        title: dato.title,
+        cuisine: dato.cuisine,
+        tags: dato.tags,
+        photoUrl: dato.photoUrl,
+        calories: dato.calories,
+      };
+      recipes.push(recipes);
+    }
+  }*/
+}
